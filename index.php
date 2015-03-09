9daaa20cbd6c4ac9303be369625358c128b08364
Paweł Marynowski <yarl@o2.pl>
2015-03-09 22:31:54 +0100

    style main loop

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 <div class="wrap">
 	<div role="main" class="row">
 		<div class="col-xs-12 col-md-8 col-lg-8">
-			<div class="box">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<?php the_post_thumbnail(array(183,183)); ?>
-				<h2>
+			
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="post post-short">
+				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(183,183)); ?></a>
+				<h3>
 					<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+				</h3>
+				<p class="post-info"><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></p>
 				<div class="entry">
-			 		<?php echo apply_filters('the_content', substr(get_the_content(), 0, 200) ); ?>
-			 	</div>
-				<?php endwhile; endif; ?>
+					<?php the_content('Czytaj dalej'); ?>
+				</div>
 			</div>
+			<?php endwhile; endif; ?>
 		</div>
 		<div class="col-xs-12 col-md-4 col-lg-4">
 			<?php get_sidebar(); ?>
