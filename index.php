09c8f285dfc9da2872d6db88924e7d477050c2ed
Paweł Marynowski <yarl@o2.pl>
2015-03-10 23:12:43 +0100

    add and style most commented articles

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 				<h3>
 					<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 				</h3>
-				<p class="post-info"><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></p>
+				<p class="post-info">autor <?php the_author_posts_link(); ?> &bull; <?php the_time('j F Y'); ?></p>
 				<div class="entry">
 					<?php the_content('Czytaj dalej'); ?>
 				</div>
@@ -17,6 +17,20 @@
 			<?php endwhile; endif; ?>
 		</div>
 		<div class="col-xs-12 col-md-4 col-lg-4">
+			<div class="widget">
+				<h2 class="widget-header">Najczęściej komentowane</h2>
+			    <?php $wp_query =
+			        new WP_Query('caller_get_posts=1&orderby=comment_count&posts_per_page=10');
+			        while ($wp_query->have_posts()) : $wp_query->the_post(); { ?>
+						<div class="post">
+							<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+							<p class="post-info">autor <?php the_author_posts_link(); ?> &bull; <?php the_time('j F Y'); ?>
+								<span class="pull-right"><?php comments_number('0 kometarzy', '1 komentarz', '% komentarzy'); ?></span>
+							</p>
+							<div class="entry"><?php echo substr(strip_tags(get_the_content()), 0, 255) ?></div>
+						</div>	
+			    <?php } endwhile; ?>
+			</div>
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
